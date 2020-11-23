ca3845dce77cfdcb40e1160ddac5728879dc71bf
Adonis <74744666+Cody-Fountain@users.noreply.github.com>
2020-11-23 14:57:43 -0500

    Update GitPy.py

--- GitPy.py
+++ GitPy.py
@@ -36,8 +36,6 @@ print(is_power_of_two(1)) # Should be True
 print(is_power_of_two(8)) # Should be True
 print(is_power_of_two(9)) # Should be False
 
-#Question 5
-
 #The multiplication_table function prints the results of a number passed to it multiplied by 1 through 5.
 #An additional requirement is that the result is not to exceed 25, which is done with the break statement.
 #Fill in the blanks to complete the function to satisfy these conditions.
